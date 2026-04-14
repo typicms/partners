69b9f1a33ba936a5dec89f314a8cd34a484e0220
Samuel De Backer <sdebacker@gmail.com>
2026-04-14 11:29:08 +0200

    Update show.blade.php

--- resources/views/public/show.blade.php
+++ resources/views/public/show.blade.php
@@ -1,8 +1,8 @@
 @extends('core::public.master')
 
 @section('title', $model->title . ' – ' . __('Partners') . ' – ' . $websiteTitle)
-@section('ogTitle', $model->title)
-@section('description', $model->summary)
+@section('ogTitle', $model->title ?? '')
+@section('description', $model->summary ?? '')
 @section('ogImage', $model->ogImageUrl())
 @section('bodyClass', 'body-partners body-partner-' . $model->id . ' body-page body-page-' . $page->id)
 
