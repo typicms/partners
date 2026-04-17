9bdb10dcd0f8483e4b465cd6277540ac44c2be7c
Samuel De Backer <sdebacker@gmail.com>
2026-04-17 14:00:11 +0200

    Unused files removed

--- resources/views/public/_json-ld.blade.php
+++
@@ -1,8 +0,0 @@
-<script type="application/ld+json">
-    {
-        "@@context": "http://schema.org",
-        "@type": "Organization",
-        "url": "{{ $partner->website }}",
-        "name": "{{ $partner->title }}"
-    }
-</script>
